ce95ec92e5cbb27675eafc2fc61342ee5f76f6af
MSCMDD <MSCAoMe@outlook.com>
2025-03-22 23:48:18 +0800

    feat: Common组件库Time增加timeZone

--- components/Common/Time/Time.vue
+++ components/Common/Time/Time.vue
@@ -8,17 +8,21 @@
 import { defineProps, computed } from 'vue'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
 import 'dayjs/locale/zh-cn'
 
 dayjs.locale('zh-cn')
 dayjs.extend(relativeTime)
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 const props = defineProps<{
     // 时间格式化字符串，详情见 format
     format?: string
     // 时间
     time?: number | Date
-    // 格式化值时使用的时区，遵循 iana time zones 格式。你可以使用 Intl.supportedValuesOf('timeZone')  来查看支持的时区
+    // 格式化值时使用的时区，遵循 iana time zones 格式。你可以使用 Intl.supportedValuesOf('timeZone')   来查看支持的时区
     timeZone?: string
     // 目标时间
     to?: number | Date
@@ -40,24 +44,36 @@ const getTimeValue = () => {
 // 格式化时间
 const formattedTime = computed(() => {
     const time = getTimeValue()
+    let dayjsObj = dayjs(time)
+    if (props.timeZone) {
+        dayjsObj = dayjsObj.tz(props.timeZone)
+    }
     if (props.format) {
-        return dayjs(time).format(props.format)
+        return dayjsObj.format(props.format)
     }
     if (props.type === 'date') {
-        return dayjs(time).format('YYYY-MM-DD')
+        return dayjsObj.format('YYYY-MM-DD')
     }
     if (props.type === 'datetime') {
-        return dayjs(time).format('YYYY-MM-DD HH:mm:ss')
+        return dayjsObj.format('YYYY-MM-DD  HH:mm:ss')
     }
-    return dayjs(time).format('YYYY-MM-DD HH:mm:ss')
+    return dayjsObj.format('YYYY-MM-DD  HH:mm:ss')
 })
 
 // 显示的时间
 const displayTime = computed(() => {
     const time = getTimeValue()
+    let dayjsObj = dayjs(time)
+    if (props.timeZone) {
+        dayjsObj = dayjsObj.tz(props.timeZone)
+    }
     if (props.type === 'relative') {
         const targetTime = props.to || Date.now()
-        return dayjs(time).from(dayjs(targetTime))
+        let targetDayjsObj = dayjs(targetTime)
+        if (props.timeZone) {
+            targetDayjsObj = targetDayjsObj.tz(props.timeZone)
+        }
+        return dayjsObj.from(targetDayjsObj)
     }
     return formattedTime.value
 })
